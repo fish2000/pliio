08309aa4f08307077b4a1244913fe2705d1fc2d4
Alexander Böhn <fish2000@gmail.com>
2014-09-06 09:57:29 -0700

    Initial commit

---
+++ ext/PyImgC/PyImgC_Constants.h
@@ -0,0 +1,11 @@
+//#include <unordered_map>
+//#include <map>
+
+#ifndef PyImgC_CONSTANTS_H
+#define PyImgC_CONSTANTS_H
+/// INSERT PYTHON C-API STUFF HERE
+
+
+
+/// END OF PYTHON C-API STUFF'S LIMINAL HASH-IFNDEF
+#endif
