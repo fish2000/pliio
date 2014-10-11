008749574bb067500ec62a5bb95f85d94c35ecb4
Alexander Böhn <fish2000@gmail.com>
2014-10-10 17:21:37 -0700

    Merged binary and unary op macros ... one less include file. Now everything is in PyImgC_IMP_Operators.h

--- pliio/ext/PyImgC_CPPSource/PyImgC_IMP_Operators.h
+++ pliio/ext/PyImgC_CPPSource/PyImgC_IMP_Operators.h
@@ -1,6 +1,25 @@
 
-#ifndef PyImgC_IMP_BINARY_OPS_H
-#define PyImgC_IMP_BINARY_OPS_H
+#ifndef PyImgC_IMP_OPERATORS_H
+#define PyImgC_IMP_OPERATORS_H
+
+#define HANDLE_UNARY_OP(type, opname, op) { \
+        auto out_img = unary_op<type>(self, op); \
+        self->assign(out_img); \
+        return reinterpret_cast<PyObject *>(self); \
+    }
+
+#define PyCImage_UNARY_OP(opname, op) \
+static PyObject *PyCImage_##opname(PyObject *smelf) { \
+    if (!smelf) { \
+        PyErr_SetString(PyExc_ValueError, \
+            "Bad argument to unary operation '##opname##'"); \
+        return NULL; \
+    } \
+    PyCImage *self = reinterpret_cast<PyCImage *>(smelf); \
+    Py_INCREF(self); \
+    SAFE_SWITCH_ON_DTYPE_FOR_UNARY_OP(self->dtype, NULL, opname, op); \
+    return NULL; \
+}
 
 #define HANDLE_BINARY_OP(type, opname, op) { \
         auto out_img = binary_op<type>(self, other, op); \
@@ -23,4 +42,4 @@ static PyObject *PyCImage_##opname(PyObject *smelf, PyObject *smother) { \
     return NULL; \
 }
 
-#endif /// PyImgC_IMP_BINARY_OPS_H
+#endif /// PyImgC_IMP_OPERATORS_H
