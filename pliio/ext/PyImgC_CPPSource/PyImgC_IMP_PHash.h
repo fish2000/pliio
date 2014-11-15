50ee55f1294efdb5ba172931af00f2d0c23b9f3e
Alexander Böhn <fish2000@gmail.com>
2014-11-14 16:29:59 -0800

    CoreGraphics cleanup, more phash functions

--- pliio/ext/PyImgC_CPPSource/PyImgC_IMP_PHash.h
+++ pliio/ext/PyImgC_CPPSource/PyImgC_IMP_PHash.h
@@ -2,10 +2,269 @@
 #ifndef PyImgC_IMP_PHASH_H
 #define PyImgC_IMP_PHASH_H
 
-#include <string>
-#include "numpypp/utils.hpp"
+#include <limits.h>
+#include <math.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#define __STDC_CONSTANT_MACROS
+#include <stdint.h>
 using namespace std;
 
+#define SQRT_TWO 1.4142135623730950488016887242097
+#define ROUNDING_FACTOR(x) (((x) >= 0) ? 0.5 : -0.5) 
+//#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+//#define EXIT_SUCCESS 1
+//#define EXIT_FAILURE 0
+
+typedef struct ph_projections {
+    CImg<uint8_t> *R;
+    int *nb_pix_perline;
+    int size;
+} Projections;
+
+typedef struct ph_feature_vector {
+    double *features;
+    int size;
+} Features;
+
+typedef struct ph_digest {
+    char *id;
+    uint8_t *coeffs;
+    int size;
+} Digest;
+
+int ph_radon_projections(const CImg<uint8_t> &img, int N, Projections &projs) {
+
+    int width = img.width();
+    int height = img.height();
+    int D = (width > height) ? width : height;
+    float x_center = static_cast<float>(width / 2);
+    float y_center = static_cast<float>(height / 2);
+    int x_off = static_cast<int>(std::floor(x_center + ROUNDING_FACTOR(x_center)));
+    int y_off = static_cast<int>(std::floor(y_center + ROUNDING_FACTOR(y_center)));
+
+    projs.R = new CImg<uint8_t>(N, D, 1, 1, 0);
+    projs.nb_pix_perline = (int *)calloc(N, sizeof(int));
+
+    if (!projs.R || !projs.nb_pix_perline) { return EXIT_FAILURE };
+
+    projs.size = N;
+
+    CImg<uint8_t> *ptr_radon_map = projs.R;
+    int *nb_per_line = projs.nb_pix_perline;
+
+    for (int k = 0; k < N / 4 + 1; k++) {
+        double theta = k * cimg::PI / N;
+        double alpha = std::tan(theta);
+        for (int x = 0; x < D; x++) {
+            double y = alpha * (x - x_off);
+            int yd = static_cast<int>(std::floor(y + ROUNDING_FACTOR(y)));
+            if ((yd + y_off >= 0) && (yd + y_off < height) && (x < width)) {
+                *ptr_radon_map->data(k, x) = img(x, yd + y_off);
+                nb_per_line[k] += 1;
+            }
+            if ((yd + x_off >= 0) && (yd + x_off < width) && (k != N / 4)
+                && (x < height)) {
+                *ptr_radon_map->data(N / 2 - k, x) = img(yd + x_off, x);
+                nb_per_line[N / 2 - k] += 1;
+            }
+        }
+    }
+    
+    int j = 0;
+    for (int k = 3 * N / 4; k < N; k++) {
+        double theta = k * cimg::PI / N;
+        double alpha = std::tan(theta);
+        for (int x = 0; x < D; x++) {
+            double y = alpha * (x - x_off);
+            int yd = static_cast<int>(std::floor(y + ROUNDING_FACTOR(y)));
+            if ((yd + y_off >= 0) && (yd + y_off < height) && (x < width)) {
+                *ptr_radon_map->data(k, x) = img(x, yd + y_off);
+                nb_per_line[k] += 1;
+            }
+            if ((y_off - yd >= 0) && (y_off - yd < width)
+                && (2 * y_off - x >= 0) && (2 * y_off - x < height)
+                && (k != 3 * N / 4)) {
+                *ptr_radon_map->data(k - j, x)
+                    = img(-yd + y_off, -(x - y_off) + y_off);
+                nb_per_line[k - j] += 1;
+            }
+        }
+        j += 2;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int ph_feature_vector(const Projections &projs, Features &fv) {
+
+    CImg<uint8_t> *ptr_map = projs.R;
+    CImg<uint8_t> projection_map = *ptr_map;
+    int *nb_perline = projs.nb_pix_perline;
+    int N = projs.size;
+    int D = projection_map.height();
+
+    fv.features = (double *)malloc(N * sizeof(double));
+    fv.size = N;
+    if (!fv.features) { return EXIT_FAILURE; }
+
+    double *feat_v = fv.features;
+    double sum = 0.0;
+    double sum_sqd = 0.0;
+    for (int k = 0; k < N; k++) {
+        double line_sum = 0.0;
+        double line_sum_sqd = 0.0;
+        int nb_pixels = nb_perline[k];
+        for (int i = 0; i < D; i++) {
+            line_sum += projection_map(k, i);
+            line_sum_sqd += projection_map(k, i) * projection_map(k, i);
+        }
+        feat_v[k] = (line_sum_sqd / nb_pixels) - (line_sum * line_sum)
+                                                 / (nb_pixels * nb_pixels);
+        sum += feat_v[k];
+        sum_sqd += feat_v[k] * feat_v[k];
+    }
+    double mean = sum / N;
+    double var = sqrt((sum_sqd / N) - (sum * sum) / (N * N));
+
+    for (int i = 0; i < N; i++) {
+        feat_v[i] = (feat_v[i] - mean) / var;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int ph_dct(const Features &fv, Digest &digest) {
+
+    int N = fv.size;
+    const int nb_coeffs = 40;
+
+    digest.coeffs = (uint8_t *)malloc(nb_coeffs * sizeof(uint8_t));
+    if (!digest.coeffs) { return EXIT_FAILURE; }
+
+    digest.size = nb_coeffs;
+    double *R = fv.features;
+    uint8_t *D = digest.coeffs;
+
+    double D_temp[nb_coeffs];
+    double max = 0.0;
+    double min = 0.0;
+    for (int k = 0; k < nb_coeffs; k++) {
+        double sum = 0.0;
+        for (int n = 0; n < N; n++) {
+            double temp = R[n] * cos((cimg::PI * (2 * n + 1) * k) / (2 * N));
+            sum += temp;
+        }
+        if (k == 0) {
+            D_temp[k] = sum / sqrt(static_cast<double>(N));
+        } else {
+            D_temp[k] = sum * SQRT_TWO / sqrt(static_cast<double>(N));
+        }
+        if (D_temp[k] > max) { max = D_temp[k]; }
+        if (D_temp[k] < min) { min = D_temp[k]; }
+    }
+
+    for (int i = 0; i < nb_coeffs; i++) {
+        D[i] = static_cast<uint8_t>(UCHAR_MAX * (D_temp[i] - min) / (max - min));
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int ph_crosscorr(const Digest &x, const Digest &y, double &pcc, double threshold=0.90) {
+
+    int N = y.size;
+    int result = 0;
+
+    uint8_t *x_coeffs = x.coeffs;
+    uint8_t *y_coeffs = y.coeffs;
+
+    double *r = new double[N];
+    double sumx = 0.0;
+    double sumy = 0.0;
+    
+    for (int i = 0; i < N; i++) {
+        sumx += x_coeffs[i];
+        sumy += y_coeffs[i];
+    }
+    
+    double meanx = sumx / N;
+    double meany = sumy / N;
+    double max = 0;
+    for (int d = 0; d < N; d++) {
+        double num = 0.0;
+        double denx = 0.0;
+        double deny = 0.0;
+        for (int i = 0; i < N; i++) {
+            num += (x_coeffs[i] - meanx) * (y_coeffs[(N + i - d) % N] - meany);
+            denx += pow((x_coeffs[i] - meanx), 2);
+            deny += pow((y_coeffs[(N + i - d) % N] - meany), 2);
+        }
+        r[d] = num / sqrt(denx * deny);
+        if (r[d] > max) max = r[d];
+    }
+    
+    delete[] r;
+    pcc = max;
+    if (max > threshold) { result = 1; }
+
+    return result;
+}
+
+int ph_image_digest(const CImg<uint8_t> &img, double sigma, double gamma,
+                     Digest &digest, int N=180) {
+
+    int result = EXIT_FAILURE;
+    CImg<uint8_t> graysc;
+
+    if (img.spectrum() >= 3) {
+        graysc = img.get_RGBtoYCbCr().channel(0);
+    } else if (img.spectrum() == 1) {
+        graysc = img;
+    } else {
+        return result;
+    }
+
+    graysc.blur((float)sigma);
+
+    (graysc / graysc.max()).pow(gamma);
+
+    Projections projs;
+    if (ph_radon_projections(graysc, N, projs) < 0) { goto cleanup; }
+
+    Features features;
+    if (ph_feature_vector(projs, features) < 0) { goto cleanup; }
+    if (ph_dct(features, digest) < 0) { goto cleanup; }
+    result = EXIT_SUCCESS;
+
+cleanup:
+    free(projs.nb_pix_perline);
+    free(features.features);
+
+    delete projs.R;
+    return result;
+}
+
+int ph_compare_images(const CImg<uint8_t> &imA, const CImg<uint8_t> &imB,
+                       double &pcc,
+                       double sigma=3.5, double gamma=1.0,
+                       int N=180, double threshold=0.90) {
+
+    int result = 0;
+    Digest digestA;
+    if (ph_image_digest(imA, sigma, gamma, digestA, N) < 0) { goto cleanup; }
+
+    Digest digestB;
+    if (ph_image_digest(imB, sigma, gamma, digestB, N) < 0) { goto cleanup; }
+    if (ph_crosscorr(digestA, digestB, pcc, threshold) < 0) { goto cleanup; }
+    if (pcc > threshold) { result = 1; }
+
+cleanup:
+    free(digestA.coeffs);
+    free(digestB.coeffs);
+    return result;
+}
 
 CImg<float> *ph_dct_matrix(const int N) {
     CImg<float> *ptr_matrix = new CImg<float>(N, N, 1, 1, 1 / sqrt((float)N));
@@ -19,6 +278,7 @@ CImg<float> *ph_dct_matrix(const int N) {
 }
 
 unsigned long long ph_dct_imagehash(CImg<uint8_t> src) {
+
     CImg<float> meanfilter(7, 7, 1, 1, 1);
     CImg<float> img;
     if (src.spectrum() == 3) {
@@ -54,6 +314,121 @@ unsigned long long ph_dct_imagehash(CImg<uint8_t> src) {
 
     delete C;
     return hash;
+}
+
+int ph_hamming_distance(const unsigned long long hash1, const unsigned long long hash2) {
+
+    unsigned long long x = hash1 ^ hash2;
+    const unsigned long long m1 = 0x5555555555555555ULL;
+    const unsigned long long m2 = 0x3333333333333333ULL;
+    const unsigned long long h01 = 0x0101010101010101ULL;
+    const unsigned long long m4 = 0x0f0f0f0f0f0f0f0fULL;
+    x -= (x >> 1) & m1;
+    x = (x & m2) + ((x >> 2) & m2);
+    x = (x + (x >> 4)) & m4;
+    return (x * h01) >> 56;
+}
+
+CImg<float> *ph_mh_kernel(float alpha, float level) {
+
+    int sigma = static_cast<int>(4 * pow((float)alpha, (float)level));
+    static CImg<float> *pkernel = NULL;
+    float xpos, ypos, A;
+    if (!pkernel) {
+        pkernel = new CImg<float>(2 * sigma + 1, 2 * sigma + 1, 1, 1, 0);
+        cimg_forXY(*pkernel, X, Y) {
+            xpos = pow(alpha, -level) * (X - sigma);
+            ypos = pow(alpha, -level) * (Y - sigma);
+            A = xpos * xpos + ypos * ypos;
+            pkernel->atXY(X, Y) = (2 - A) * exp(-A / 2);
+        }
+    }
+    return pkernel;
+}
+
+uint8_t *ph_mh_imagehash(CImg<uint8_t> src, float alpha=2.0f, float lvl = 1.0f) {
+
+    uint8_t *hash = (unsigned char *)malloc(72 * sizeof(uint8_t));
+    CImg<uint8_t> img;
+    
+    if (src.spectrum() == 3) {
+        img = src.get_RGBtoYCbCr()
+                  .channel(0)
+                  .blur(1.0)
+                  .resize(512, 512, 1, 1, 5)
+                  .get_equalize(256);
+    } else {
+        img = src.get_channel(0)
+                  .get_blur(1.0)
+                  .resize(512, 512, 1, 1, 5)
+                  .get_equalize(256);
+    }
+    src.clear();
+
+    CImg<float> *pkernel = ph_mh_kernel(alpha, lvl);
+    CImg<float> fresp = img.get_correlate(*pkernel);
+    img.clear();
+    fresp.normalize(0, 1.0);
+    CImg<float> blocks(31, 31, 1, 1, 0);
+    for (int rindex = 0; rindex < 31; rindex++) {
+        for (int cindex = 0; cindex < 31; cindex++) {
+            blocks(rindex, cindex)
+                = fresp.get_crop(rindex * 16, cindex * 16, rindex * 16 + 16 - 1,
+                                 cindex * 16 + 16 - 1).sum();
+        }
+    }
+    int hash_index;
+    int nb_ones = 0, nb_zeros = 0;
+    int bit_index = 0;
+    unsigned char hashbyte = 0;
+    for (int rindex = 0; rindex < 31 - 2; rindex += 4) {
+        CImg<float> subsec;
+        for (int cindex = 0; cindex < 31 - 2; cindex += 4) {
+            subsec = blocks.get_crop(cindex, rindex, cindex + 2, rindex + 2)
+                         .unroll('x');
+            float ave = subsec.mean();
+            cimg_forX(subsec, I) {
+                hashbyte <<= 1;
+                if (subsec(I) > ave) {
+                    hashbyte |= 0x01;
+                    nb_ones++;
+                } else {
+                    nb_zeros++;
+                }
+                bit_index++;
+                if ((bit_index % 8) == 0) {
+                    hash_index = static_cast<int>((bit_index / 8) - 1);
+                    hash[hash_index] = hashbyte;
+                    hashbyte = 0x00;
+                }
+            }
+        }
+    }
+    return hash;
+}
+
+int ph_bitcount8(uint8_t val) {
+
+    int num = 0;
+    while (val) {
+        ++num;
+        val &= val - 1;
+    }
+    return num;
+}
+
+double ph_hamming_distance2(uint8_t *hashA, uint8_t *hashB, int lenA=72, int lenB=72) {
+
+    if (lenA != lenB) { return -1.0; }
+    if ((hashA == NULL) || (hashB == NULL) || (lenA <= 0)) { return -1.0; }
+    double dist = 0;
+    uint8_t D = 0;
+    for (int i = 0; i < lenA; i++) {
+        D = hashA[i] ^ hashB[i];
+        dist = dist + static_cast<double>(ph_bitcount8(D));
+    }
+    double bits = static_cast<double>(lenA) * 8;
+    return dist / bits;
 }
 
 #endif /// PyImgC_IMP_PHASH_H
